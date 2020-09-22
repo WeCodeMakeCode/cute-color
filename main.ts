aae2501cb7c04cae460abe50f3e9a68b19c05ccd
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-09-22 10:56:10 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ namespace cuteColor{
     export function colorIndexPicker(color:number){
         return color
     }
-        /**
+    /**
      * returns a random color 0-15 excluding up to 3 colors
      */
     //     export function randomColor(n:number = 2, c1:number = 0, c2: number = 15, c3:number = 0): number{
@@ -20,9 +20,9 @@ namespace cuteColor{
     //% block=" random color || excluding %n colors: %c1 %c2 %c3"
     //% inlineInputMode=inline
     //% n.min=0 n.max=3 n.defl=2
-    //% c1.min=0 c1.max=15 c1.defl=0
-    //% c2.min=0 c2.max=15 c2.defl=15
-    //% c3.min=0 c3.max=15 c3.defl=0
+    //% c1.min=0 c1.max=15 //c1.defl=0
+    //% c2.min=0 c2.max=15 //c2.defl=15
+    //% c3.min=0 c3.max=15 // c3.defl=0
     //% help=circle/random-color
     export function randomColor(c1:number = null, c2: number = null, c3:number = null ): number{
         let c:number[] = []
