a9a3b54e59679ade419f663adc092b4675868ef5
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-10-06 10:40:36 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -21,48 +21,72 @@ namespace cuteColor{
         return s
     }
     /**
-     * returns a colors array
-     */
-    //% blockId=colorsArray
-    //% block
-    export function colorsArray(): number[]{
-        return [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    }
-        /**
-     * returns a color names array
-     */
-    //% blockId=colorNamesArray
-    //% block
-    export function colorNamesArray(): string[]{
-        return ["transparent","white","red","pink","orange","yellow","teal","green","blue","light blue","purple","light purple","dark purple","tan","brown","black"]
-    }
-    /**
      * returns a random color 0-15 excluding up to 3 colors
      */
     //% blockId=randomColor
     //% block=" random color || excluding colors: %c1 %c2 %c3"
     //% inlineInputMode=inline
-    export function randomColor(c1:string = "", c2:string = "", c3:string = ""): number{
-        let cInts =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-        let cIntsText =["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
-        let cNamesText =["transparent","white","red","pink","orange","yellow","teal","green","blue","light blue","purple","light purple","dark purple","tan","brown","black"]
-        // remove colors to be excluded
-        let i1 = colorNumber(c1, cIntsText, cNamesText)
-        let i2 = colorNumber(c2, cIntsText, cNamesText)
-        let i3 = colorNumber(c3, cIntsText, cNamesText)
-        if (i1 > -1) cInts.removeElement(i1)
-        if (i2 > -1) cInts.removeElement(i2)
-        if (i3 > -1) cInts.removeElement(i3)
-
-        return Math.pickRandom(cInts)
+    export function randomColor(c1:number = -1, c2:number = -1, c3:number = -1): number{
+        let cs = new CuteColor()
+        cs.excludeColorNumber(c1)
+        cs.excludeColorNumber(c2)
+        cs.excludeColorNumber(c3)
+        return Math.pickRandom(cs.colorNumbers)
     }
-    function colorNumber(c:string, ar1:string[], ar2:string[]):number {
-        if (ar1.indexOf(c) > -1) {
-            return ar1.indexOf(c)     
-        } else if (ar2.indexOf(c) > -1) {
-            return ar2.indexOf(c)
+    class CuteColor{
+        private _colorNames: string[] = []
+        private _colorNumbers: number[] = []
+        constructor(){
+            this._colorNames =["transparent","white","red","pink","orange","yellow","teal","green","blue","light blue","purple","light purple","dark purple","tan","brown","black"]
+            this._colorNumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
         }
-        return -1
+        //% blockId=colorNamesArray
+        //% blockSetVariable=myColorNamesArray
+        //% block
+        get colorNames(): string[] {
+            return this._colorNames
+        }
+        //% blockId=colorNumbersArray
+        //% blockSetVariable=myColorNumbersArray
+        //% block
+        get colorNumbers(): number[] {
+            return this._colorNumbers
+        }
+        //% blockId=excludeColorNumber
+        //% blockSetVariable=myBoolean
+        //% block %c
+        public excludeColorNumber(c:number): boolean {
+            let i = this._colorNumbers.indexOf(c)
+            if (i > -1) {
+                this._colorNumbers.removeAt(i)
+                this._colorNames.removeAt(i)
+                return true
+            } else return false
+        }
+        //% blockId=excludeColorName
+        //% blockSetVariable=myBoolean
+        //% block %cName
+        public excludeColorName(cName:string): boolean {
+            let i = this._colorNames.indexOf(cName) 
+            if ( i > -1) {
+                this._colorNumbers.removeAt(i)
+                this._colorNames.removeAt(i)
+                return true
+            } else return false
+        }
+        //% blockId=colorNumberFromName
+        //% blockSetVariable=myNumber
+        //% block %colorName
+        public colorNumberFromName(colorName:string):number {
+            return this._colorNames.indexOf(colorName)
+        }
+        //% blockId=colorNameFromNumber
+        //% blockSetVariable=myString
+        //% block %colorNumber
+        public colorNameFromNumber(colorNumber:number):string {
+            return this._colorNames[colorNumber]
+        }
+       
     }
 }
 
